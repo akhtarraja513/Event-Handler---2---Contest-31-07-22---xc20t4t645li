4551a9340802b2b54a59a1a92011388794ec75f4
akhtarraja513 <akhtarraja513@gmail.com>
2022-07-31 18:19:55 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,20 @@ const App = () => {
 
   const handleClick = (event) =>{
     // use console.log
+    if(event.target.id === 'button-a') {
+      console.log('Button id is:-button-a');
+    } else if(event.target.id === 'button-b') {
+      console.log('Button id is:-button-b');
+    }
+
 
   }
 
   // do not remove the two buttons or change their id
   return (
     <div id="main">
-      <button id="button-a">Button A</button>
-      <button id="button-b">Button B</button>
+      <button onClick={handleClick} id="button-a">Button A</button>
+      <button onClick={handleClick} id="button-b">Button B</button>
     </div>
   )
 }
